c483e19e7c297d853b5b64599c5110ac55fb119f
ST-GuY <estebanlvy23@gmail.com>
2026-01-14 11:26:40 +0100

    Printing teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,6 +11,10 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -27,5 +31,13 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe'));
 console.log(director1);
 console.log(teacher3);
