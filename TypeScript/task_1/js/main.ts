276951a06a0b3f12ee26721967593af69cbec1f7
ST-GuY <estebanlvy23@gmail.com>
2026-01-14 11:15:31 +0100

    corresction lastName

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,18 +1,31 @@
 interface Teacher {
 	readonly firstName: string;
-	readonly lastname: string;
+	readonly lastName: string;
 	fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
 	location: string;
 	[key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
-  lastname: 'Joe',
-  fullTimeEmployee: false,
   lastName: 'Doe',
+  fullTimeEmployee: false,
   location: 'London',
   contract: false,
 };
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
 console.log(teacher3);
