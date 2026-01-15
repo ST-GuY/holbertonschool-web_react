cc0e8df02912e84aae9bee9278b4e17bf82b3adb
ST-GuY <estebanlvy23@gmail.com>
2026-01-15 15:10:24 +0100

    String literal types

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -51,6 +51,16 @@ function executeWork(employee: Director | Teacher): string {
   return employee.workTeacherTasks();
 }
 
+type Subjects = 'Math' | 'History';
 
+function teachClass(todayClass: Subjects): string {
+	if (todayClass === 'Math') {
+		return 'Teaching Math';
+	}
+	return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
